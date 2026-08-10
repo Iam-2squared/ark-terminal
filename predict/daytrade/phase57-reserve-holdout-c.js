f912e531b30981e218d52b9f9c77b7b139086c71
Iam2squared <yosuke.gw@gmail.com>
2026-08-10 23:30:45 +0900

    Pre-register independent reserve holdout C universe

---
+++ predict/daytrade/phase57-reserve-holdout-c.js
@@ -0,0 +1,17 @@
+export const P23_12_RESERVE_HOLDOUT_C_SYMBOLS=Object.freeze([
+  '1925.T','1928.T','2502.T','2503.T','2802.T','3402.T','3407.T','3861.T','4188.T','4452.T',
+  '4612.T','4911.T','5019.T','5201.T','5301.T','5332.T','5333.T','5801.T','5802.T','6113.T',
+  '6273.T','6305.T','6471.T','6504.T','6506.T','6586.T','6674.T','6703.T','6845.T','6920.T',
+]);
+export const P23_12_RESERVE_HOLDOUT_C_POLICY=Object.freeze({
+  status:'UNIVERSE_RESERVED_BEFORE_ARCHITECTURE_SELECTION_AND_BEFORE_OUTCOME_RETRIEVAL',
+  symbolCount:30,
+  description:'Independent reserve Japanese-equity historical holdout basket C; not claimed to be a current index or liquidity ranking.',
+  overlapWithDevelopment30Forbidden:true,
+  overlapWithHoldoutAForbidden:true,
+  overlapWithHoldoutBForbidden:true,
+  outcomeRetrievalAllowed:false,
+  architectureNotYetAssigned:true,
+  noOutcomeBasedUniverseChanges:true,
+});
+export default {P23_12_RESERVE_HOLDOUT_C_SYMBOLS,P23_12_RESERVE_HOLDOUT_C_POLICY};
